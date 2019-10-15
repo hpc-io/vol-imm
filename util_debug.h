04f47a605cc7c4fa89cafea21e4aae2862bb477f
Tonglin Li <tonglinli@lbl.gov>
2019-10-15 16:23:20 -0500

    Cleaning up debug info

--- util_debug.h
+++ util_debug.h
@@ -12,6 +12,6 @@
 #include <stdio.h>
 
 int MY_RANK_DEBUG;
-#define DEBUG_PRINT  printf("%s:%u, rank = %d\n", __func__, __LINE__, MY_RANK_DEBUG);
+#define DEBUG_PRINT  //printf("%s:%u, rank = %d\n", __func__, __LINE__, MY_RANK_DEBUG);
 
 #endif /* UTIL_DEBUG_H_ */
